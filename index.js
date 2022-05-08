0bb96321a07eab26137cc4adb71278ee7b10634b
Nazmul Alam <nazmuldipu@gmail.com>
2022-05-08 22:04:13 +0600

    demo server added

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 2087;
-const winston = require("winston");
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 2087;
+// const winston = require("winston");
 
-require("./startup/logging")();
-require("./startup/routes")(app);
-require("./startup/db")();
+// require("./startup/logging")();
+// require("./startup/routes")(app);
+// require("./startup/db")();
 
-app.get("/", (req, res) => {
-  res.send({ message: "Weclome to Node REST API" });
-});
+// app.get("/", (req, res) => {
+//   res.send({ message: "Weclome to Node REST API" });
+// });
 
 // const server = app.listen(port, () => {
 //   winston.info(`Listening on port ${port}...`);
 // });
 
 // module.exports = server;
+// // 
 
-app.listen(port, () => console.log(`Server is running in port ${port}`));
+const express = require("express");
+const app = express();
+const users = require("./routes/users");
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", users);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
